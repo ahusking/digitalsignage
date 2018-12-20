7bbada83676327e1c8de423f5cc68eab5a2e0c23
Andrew Husking <andrew@husking.id.au>
2018-12-20 20:47:42 +1100

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo cleanup apps we dont need
+sudo apt-get remove --purge wolfram-engine scratch nuscratch sonic-pi idle3 smartsim java-common minecraft-pi python-minecraftpi python3-minecraftpi
+echo update apt repos
+sudo apt-get update
+echo install extra tools for signage
+apt-get install xdotool unclutter chromium-browser
+echo make directories for autostart
+mkdir ~/.config/autostart
+echo download autostart config
+curl -o ~/.config/autostart https://raw.githubusercontent.com/ahusking/digitalsignage/master/kiosk.desktop
+echo download kiosk script
+curl -o ~/ https://raw.githubusercontent.com/ahusking/digitalsignage/master/kiosk.sh
+bash ~/kiosk.sh
